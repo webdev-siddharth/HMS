a0240bc4148959d97a9c071eeeff3f7bf76e9f2c
Siddharth Vishwakarma <webdev.siddharthv@gmail.com>
2025-04-06 16:32:33 +0530

    Edited func.php back to original

--- func3.php
+++ func3.php
@@ -1,64 +1,52 @@
 <?php
 session_start();
-
-// Connect to NeonDB (PostgreSQL)
-$conn_string = getenv("DB_CONNECTION_STRING");
-
-$con = pg_connect($conn_string);
-
-if (!$con) {
-    die("Connection failed: " . pg_last_error());
+$con=mysqli_connect("localhost","root","","myhmsdb");
+if(isset($_POST['adsub'])){
+	$username=$_POST['username1'];
+	$password=$_POST['password2'];
+	$query="select * from admintb where username='$username' and password='$password';";
+	$result=mysqli_query($con,$query);
+	if(mysqli_num_rows($result)==1)
+	{
+		$_SESSION['username']=$username;
+		header("Location:admin-panel1.php");
+	}
+	else
+		// header("Location:error2.php");
+		echo("<script>alert('Invalid Username or Password. Try Again!');
+          window.location.href = 'index.php';</script>");
+}
+if(isset($_POST['update_data']))
+{
+	$contact=$_POST['contact'];
+	$status=$_POST['status'];
+	$query="update appointmenttb set payment='$status' where contact='$contact';";
+	$result=mysqli_query($con,$query);
+	if($result)
+		header("Location:updated.php");
 }
 
-// Admin Login
-if (isset($_POST['adsub'])) {
-    $username = $_POST['username1'];
-    $password = $_POST['password2'];
 
-    $query = "SELECT * FROM admintb WHERE username = '$username' AND password = '$password'";
-    $result = pg_query($con, $query);
 
-    if (pg_num_rows($result) == 1) {
-        $_SESSION['username'] = $username;
-        header("Location:admin-panel1.php");
-    } else {
-        echo "<script>alert('Invalid Username or Password. Try Again!');
-        window.location.href = 'index.php';</script>";
-    }
-}
 
-// Update appointment payment status
-if (isset($_POST['update_data'])) {
-    $contact = $_POST['contact'];
-    $status = $_POST['status'];
-
-    $query = "UPDATE appointmenttb SET payment = '$status' WHERE contact = '$contact'";
-    $result = pg_query($con, $query);
-
-    if ($result)
-        header("Location:updated.php");
-}
-
-// Display doctors
 function display_docs()
 {
-    global $con;
-    $query = "SELECT * FROM doctb";
-    $result = pg_query($con, $query);
-
-    while ($row = pg_fetch_array($result)) {
-        $name = $row['name'];
-        echo '<option value="' . $name . '">' . $name . '</option>';
-    }
+	global $con;
+	$query="select * from doctb";
+	$result=mysqli_query($con,$query);
+	while($row=mysqli_fetch_array($result))
+	{
+		$name=$row['name'];
+		# echo'<option value="" disabled selected>Select Doctor</option>';
+		echo '<option value="'.$name.'">'.$name.'</option>';
+	}
 }
 
-// Add a doctor
-if (isset($_POST['doc_sub'])) {
-    $name = $_POST['name'];
-    $query = "INSERT INTO doctb (name) VALUES ('$name')";
-    $result = pg_query($con, $query);
-
-    if ($result)
-        header("Location:adddoc.php");
+if(isset($_POST['doc_sub']))
+{
+	$name=$_POST['name'];
+	$query="insert into doctb(name)values('$name')";
+	$result=mysqli_query($con,$query);
+	if($result)
+		header("Location:adddoc.php");
 }
-?>
